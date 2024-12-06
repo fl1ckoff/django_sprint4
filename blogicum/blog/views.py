f3821952811cbdfbd574002a6c0dc42c42e94cf6
stoicismguy <pyzan55@gmail.com>
2024-12-06 17:55:50 +0500

    changes

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -100,7 +100,7 @@ class AuthorProfileListView(PostsQuerySetMixin, ListView):
     template_name = "blog/profile.html"
     paginate_by = PAGINATED_BY
 
-    def get_queryset(self):
+    def get_queryset(self, **kwargs):
         if self.request.user.username == self.kwargs["username"]:
             return (
                 self.request.user.posts.select_related(
